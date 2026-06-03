427419cd4feae9fc0f8851d1b9257ca068a1cffd
Faisal-75 <faisalnabulsi75@gmail.com>
2026-06-03 17:36:33 +0200

    Add files via upload

---
+++ MQTT_SEND_SAT_80.py
@@ -0,0 +1,39 @@
+import paho.mqtt.client as mqtt
+import time
+
+# Connection details
+MQTT_BROKER = "mqtt.ics.ele.tue.nl"
+
+USERNAME = "robot_80_1"
+PASSWORD = "OQfY8Km2"
+
+PUBLISH_TOPIC = "/pynqbridge/80/recv"
+
+# Setup MQTT client
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+
+client.username_pw_set(USERNAME, PASSWORD)
+
+# Connect
+print(f"Connecting to {MQTT_BROKER}...")
+
+client.connect(MQTT_BROKER, 1883, 60)
+
+client.loop_start()
+
+time.sleep(1)
+
+print("CONNECTED!")
+
+# Main loop
+while True:
+
+    message_text = input("ENTER COMMAND: ")
+
+    print(f"Sending '{message_text}'")
+
+    client.publish(PUBLISH_TOPIC, message_text)
+
+    print("Message sent!\n")
+
+    time.sleep(0.2)
